aecbf69a9793c9a2f67c288eb260d43b40338e21
erand-palliyaguru <erandasampath2000@gmail.com>
2021-05-25 11:53:10 +0530

    up

--- foote.php
+++ foote.php
@@ -9,26 +9,14 @@
                   <h3 class="ps-widget__title">Address Office 1</h3>
                 </header>
                 <footer>
-                  <p><strong>460 West 34th Street, 15th floor, New York</strong></p>
-                  <p>Email: <a href='mailto:support@store.com'>support@store.com</a></p>
-                  <p>Phone: +323 32434 5334</p>
-                  <p>Fax: ++323 32434 5333</p>
+                  <p><strong>45/8,High level road , Homagama</strong></p>
+                  <p>Email: <a href='mailto:kiriwaththuduwastores67@gmail.com'>kiriwaththuduwastores67@gmail.com</a></p>
+                  <p>Phone: 0112 857797</p>
+
                 </footer>
               </aside>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
-              <aside class="ps-widget--footer ps-widget--info second">
-                <header>
-                  <h3 class="ps-widget__title">Address Office 2</h3>
-                </header>
-                <footer>
-                  <p><strong>PO Box 16122 Collins  Victoria 3000 Australia</strong></p>
-                  <p>Email: <a href='mailto:support@store.com'>support@store.com</a></p>
-                  <p>Phone: +323 32434 5334</p>
-                  <p>Fax: ++323 32434 5333</p>
-                </footer>
-              </aside>
-            </div>
+
             <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12 ">
               <aside class="ps-widget--footer ps-widget--link">
                 <header>
@@ -39,7 +27,7 @@
                     <li><a href="#">Coupon Code</a></li>
                     <li><a href="#">SignUp For Email</a></li>
                     <li><a href="#">Site Feedback</a></li>
-                    
+
                   </ul>
                 </footer>
               </aside>
